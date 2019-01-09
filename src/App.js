0c6bef8c9c52ecccddbd4fcaafc8cdd385ce5f4b
olegchursin <oleg.chursin@aon.com>
2019-01-09 10:43:53 -0500

    Various implementations

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import i18n from './i18n';
+import { withNamespaces, NamespacesConsumer, Trans } from 'react-i18next';
+
+function Welcome() {
+  return (
+    <NamespacesConsumer>
+      {
+        (t, { i18n }) => <h2>{t('title')}</h2>
+      }
+    </NamespacesConsumer>
+  )
+}
+
+const MyComponent = ({ t }) => {
+  return (
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+};
+const MyComponentWrapped = withNamespaces()(MyComponent);
 
 class App extends Component {
   render() {
-    const lng = 'es'
+    const { t, i18n } = this.props;
+
+    const changeLanguage = lng => {
+      console.log('changing // lng:', lng)
+      i18n.changeLanguage(lng);
+    };
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1>{i18n.t('Welcome', {lng})}</h1>
+          <Welcome />
           <div>
-            <ul>
-              <li>{i18n.t('name.label', {lng})}</li>
-              <li>{i18n.t('age.label', {lng})}</li>
-              <li>{i18n.t('home.label', {lng})}</li>
-            </ul>
+            <button onClick={() => changeLanguage('de')}>de</button>
+            <button onClick={() => changeLanguage('en')}>en</button>
           </div>
+          <div>
+            <MyComponentWrapped />
+          </div>
+          <div>{t('description.part2')}</div>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withNamespaces('translation')(App);
--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>,
+  document.getElementById('root')
+);
