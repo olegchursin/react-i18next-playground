76ab4b684b9bbcc41fa2d3ec0683022fb3b0a226
olegchursin <oleg.chursin@aon.com>
2019-01-09 03:05:37 -0500

    Way to change lang

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import i18n from './i18n';
 
 class App extends Component {
   render() {
+    const lng = 'es'
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1>{i18n.t('Welcome')}</h1>
+          <h1>{i18n.t('Welcome', {lng})}</h1>
           <div>
             <ul>
-              <li>{i18n.t('name.label')}</li>
-              <li>{i18n.t('age.label')}</li>
-              <li>{i18n.t('home.label')}</li>
+              <li>{i18n.t('name.label', {lng})}</li>
+              <li>{i18n.t('age.label', {lng})}</li>
+              <li>{i18n.t('home.label', {lng})}</li>
             </ul>
           </div>
         </header>
